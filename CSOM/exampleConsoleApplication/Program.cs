7ba31b0bb6fa8d5803167f42a5b92891e431cc2c
Adam <adam.wojcik.it@gmail.com>
2020-07-05 19:48:42 +0200

    added  a .NET core application that uses CSOM for .NET Standard and modern authentication to SharePoint via Azure AD

---
+++ CSOM/exampleConsoleApplication/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security;
+using System.Threading.Tasks;
+using CSOMDemo;
+using Microsoft.SharePoint.Client;
+
+namespace exampleConsoleApplication
+{
+    class Program
+    {
+        private const string _siteUrl = "[SiteUrl]";
+        private const string _userLogin = "[UserLogin]";
+        private const string _listName = "[ListName]";
+
+        public static async Task Main(string[] args)
+        {
+            Uri site = new Uri(_siteUrl);
+            SecureString password = GetSecureString($"Password for {_userLogin}");
+
+            using (var authenticationManager = new AuthenticationManager())
+            using (var context = authenticationManager.GetContext(site, _userLogin, password))
+            {
+                context.Load(context.Web, p => p.Title);
+                await context.ExecuteQueryAsync();
+                Console.WriteLine($"Title: {context.Web.Title}");
+
+                var web = context.Web;
+                var list = web.Lists.GetByTitle(_listName);
+                context.Load(list, includes => includes.Title);
+                await context.ExecuteQueryAsync();
+                Console.WriteLine($"List Title: {list.Title}");
+
+                ListItemCreationInformation itemCreateInfo = new ListItemCreationInformation();
+                ListItem item = list.AddItem(itemCreateInfo);
+                item["Title"] = "New Item Added Via CSOM";
+                item.Update();
+                await context.ExecuteQueryAsync();
+            }
+        }
+
+        public static SecureString GetSecureString(string message)
+        {
+            SecureString securePwd = new SecureString();
+           
+            Console.WriteLine(message);
+            var password = Console.ReadLine();
+
+            foreach(var ch in password)
+            {
+                securePwd.AppendChar(ch);
+            }
+
+            return securePwd;
+        }
+    }
+}
+
---
+++ CSOM/exampleConsoleApplication/README.md
@@ -0,0 +1,18 @@
+## exampleConsoleApplication
+
+#### Description
+
+This project is a .NET Core console app that uses SharePoint Online CSOM for .NET Standard. The app has an example implementation of modern authentication based on tokens and autorization via Azure Active Directory App registration. The app queries a list and add one item to it
+
+----
+#### MSDN 
+
+MSDN resource helpful to understand the used technology and modern authentication with CSOM for .NET Standard.
+
+https://docs.microsoft.com/en-us/sharepoint/dev/sp-add-ins/using-csom-for-dotnet-standard
+
+---
+#### Example
+
+permission added in Azure AD for this app registration
+![](../Images/exampleConsoleApplicationPermissionsInAzureAD.png)
