bd1b5c9ec02c76eaaa6e2e5444109e92414d5745
Adam <adam.wojcik.it@gmail.com>
2020-08-01 14:00:17 +0200

    fix

---
+++ TeamsSolutions/spfx/SPfxTeamsTabWebpart/src/webparts/teamsTabSample/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITeamsTabSampleWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TeamsTabSampleWebPartStrings' {
+  const strings: ITeamsTabSampleWebPartStrings;
+  export = strings;
+}
