03c0cf32abc87984943f043b0a29648ba40419c0
Adam <adam.wojcik.it@gmail.com>
2021-04-26 23:35:31 +0200

    sample console application that uses PnP.Core nuget as CSOM that uses SP rest or Graph to get sharepoint lists

---
+++ CSOM/PnPCoreConsoleApplication/PnPCoreConsoleApplication/PnPCoreConsoleApplication/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using PnP.Core.QueryModel;
+using PnP.Core.Services;
+using PnP.Core.Services.Builder.Configuration;
+using PnP.Core.Auth.Services.Builder.Configuration;
+using System.Linq;
+
+namespace PnPCoreConsoleApplication
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder()
+            // Configure logging
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddEventSourceLogger();
+                logging.AddConsole();
+            })
+            // Configure services
+            .ConfigureServices((hostingContext, services) =>
+            {
+                // Add the PnP Core SDK library services
+                services.AddPnPCore();
+                // Add the PnP Core SDK library services configuration from the appsettings.json file
+                services.Configure<PnPCoreOptions>(hostingContext.Configuration.GetSection("PnPCore"));
+                // Add the PnP Core SDK Authentication Providers
+                services.AddPnPCoreAuthentication();
+                // Add the PnP Core SDK Authentication Providers configuration from the appsettings.json file
+                services.Configure<PnPCoreAuthenticationOptions>(hostingContext.Configuration.GetSection("PnPCore"));
+            })
+            // Let the builder know we're running in a console
+            .UseConsoleLifetime()
+            // Add services to the container
+            .Build();
+
+            await host.StartAsync();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var pnpContextFactory = scope.ServiceProvider.GetRequiredService<IPnPContextFactory>();
+
+                using (var context = await pnpContextFactory.CreateAsync("DevSite"))
+                {
+                    var web = await context.Web.GetAsync(p => p.Title, p => p.Lists, p => p.MasterUrl);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("===Web (REST)===");
+                    Console.WriteLine($"Title: {web.Title}");
+                    Console.WriteLine($"# Lists: {web.Lists.Length}");
+                    Console.WriteLine($"Master page url: {web.MasterUrl}");
+                    Console.ResetColor();
+
+                    // We can retrieve the whole list of lists 
+                    // and their items in the context web
+                    var listsQuery = (from l in context.Web.Lists.QueryProperties(l => l.Id, l => l.Title, l => l.Description)
+                                      orderby l.Title descending
+                                      select l);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("===LINQ: Retrieve list and list items===");
+                    foreach (var list in listsQuery.ToList())
+                    {
+                        Console.WriteLine($"{list.Id} - {list.Title} - Items count: {list.Items.Length}");
+                    }
+                    Console.ResetColor();
+                }
+            }
+
+            host.Dispose();
+        }
+    }
+}
---
+++ CSOM/PnPCoreConsoleApplication/PnPCoreConsoleApplication/PnPCoreConsoleApplication/README.md
@@ -0,0 +1,24 @@
+﻿## PnPCoreConsoleApplication
+
+#### Description
+
+This project is a .NET Core console app that uses PnP.Core in .NET Core 3.1. In order for the app to work we need to register it in Azure AD with the required permissions for SharePoint and Graph defined in the spec.
+
+----
+#### MSDN 
+
+PnP resource helpful to understand the used technology and modern authentication with CSOM for .NET Core 3.1.
+
+https://pnp.github.io/pnpcore/using-the-sdk/configuring%20authentication.html
+
+---
+#### Example
+
+app settings needs to be copied to output
+![](../../../Images/AppSettings.png)
+
+nugets used in the project
+![](../../../Images/Nugets.png)
+
+output of the console app
+![](../../../Images/outputOfPnPCoreApp.png)
